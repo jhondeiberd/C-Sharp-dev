877bfad5507f96af39bd59355d8032c45b815141
jhondeiberd <jhondeiberd@gmail.com>
2022-12-30 11:25:33 -0500

    Variables

---
+++ Tips/Developer/Developer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+
+            //VARIABLES
+            //int a = 10;
+            //long i = 1;
+            //double e = 10.5;
+            //bool p = false;
+            //ulong ul = 1ul; 
+            //float d = 10.5f;
+            //decimal de = 1.0m;
+            //char b = 'a';
+            //string c = "Hello";
+            //const float pi = 3.1416f;
+
+            //CONVERTION
+            //int a = 10;
+            //double b = 10.5;
+            //a = (int)b;
+            //b = a;
+            //double x = 12.89;
+            //long y = (long)x;
+            
+        }
+
+
+    }
+}
