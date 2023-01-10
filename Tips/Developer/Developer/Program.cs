20e1aeb21eaefcc4338bdb018a681c182df4783a
jhondeiberd <jhondeiberd@gmail.com>
2023-01-09 23:01:44 -0500

    Event

--- Tips/Developer/Developer/Program.cs
+++ Tips/Developer/Developer/Program.cs
@@ -228,21 +228,6 @@ namespace Developer
             Console.WriteLine("The total price of all books is: {0}", totalBook.total);
             Console.ReadKey();
             */
-
-            /*EVENT
-            Circle c1 = new Circle(54);
-            Circle c2 = new Circle(88);
-
-            c1.CircleChanged += HandlerChangedCircle;
-            c2.CircleChanged += HandlerChangedCircle;
-
-            c1.Draw();
-            c2.Draw();
-
-            c1.Update(57);
-            c2.Update(98);
-            Console.ReadKey();
-             */
             
         }
         /*DELEGATE
@@ -276,55 +261,7 @@ namespace Developer
         }
         */
         
-        /*EVENT
-        private static void HandlerChangedCircle(object sender, CircleEventArgs AreaChanged)
-        {
-            Circle s = (Circle)sender;
-            Console.WriteLine("New Event. The area of circle now is : {0}", AreaChanged.NewArea);
-            s.Draw();
-        }
-         */
     }
-
-    /*EVENT
-    public class Circle
-    {
-        private double radius;
-        public double Area { get; set; }
-        public Circle(double rad)
-        {
-            radius = rad;
-            Area = 3.1416 * radius * radius;
-        }
-        public void Update(double rad)
-        {
-            radius = rad;
-            Area = 3.1416 * radius * radius;
-            OnChangedCircle(new CircleEventArgs(Area));
-        }
-        public void Draw()
-        {
-            Console.WriteLine("Drawing a circle");
-        }
-        public event EventHandler<CircleEventArgs> CircleChanged;
-        protected void OnChangedCircle(CircleEventArgs AreaChanged)
-        {
-            EventHandler<CircleEventArgs> Handler = CircleChanged;
-            if (Handler != null)
-            {
-                Handler(this, AreaChanged);
-            }
-        }
-    }
-    public class CircleEventArgs : EventArgs
-    {
-        public double NewArea { get; set; }
-        public CircleEventArgs(double rad)
-        {
-            NewArea = rad;
-        }
-    }
-    */
 
     /*DELEGATE
     public delegate void Del(string message);
---
+++ Tips/Event/Event/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(54);
+            Circle c2 = new Circle(88);
+
+            c1.CircleChanged += HandlerChangedCircle;
+            c2.CircleChanged += HandlerChangedCircle;
+
+            c1.Draw();
+            c2.Draw();
+
+            c1.Update(57);
+            c2.Update(98);
+            Console.ReadKey();
+        }
+        private static void HandlerChangedCircle(object sender, CircleEventArgs AreaChanged)
+        {
+            Circle s = (Circle)sender;
+            Console.WriteLine("New Event. The area of circle now is : {0}", AreaChanged.NewArea);
+            s.Draw();
+        }
+    }
+    public class Circle
+    {
+        private double radius;
+        public double Area { get; set; }
+        public Circle(double rad)
+        {
+            radius = rad;
+            Area = 3.1416 * radius * radius;
+        }
+        public void Update(double rad)
+        {
+            radius = rad;
+            Area = 3.1416 * radius * radius;
+            OnChangedCircle(new CircleEventArgs(Area));
+        }
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+        }
+        public event EventHandler<CircleEventArgs> CircleChanged;
+        protected void OnChangedCircle(CircleEventArgs AreaChanged)
+        {
+            EventHandler<CircleEventArgs> Handler = CircleChanged;
+            if (Handler != null)
+            {
+                Handler(this, AreaChanged);
+            }
+        }
+    }
+    public class CircleEventArgs : EventArgs
+    {
+        public double NewArea { get; set; }
+        public CircleEventArgs(double rad)
+        {
+            NewArea = rad;
+        }
+    }
+}
