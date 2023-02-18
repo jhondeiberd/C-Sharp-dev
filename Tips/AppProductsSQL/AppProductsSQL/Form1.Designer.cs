ad8088e0dd1cdf2762c084db1488204e63ccf0cc
jhondeiberd <jhondeiberd@gmail.com>
2023-02-17 21:23:07 -0500

    Update

---
+++ Tips/AppProductsSQL/AppProductsSQL/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AppProductsSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewProducts = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.viewProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // viewProducts
+            // 
+            this.viewProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewProducts.Location = new System.Drawing.Point(30, 43);
+            this.viewProducts.Name = "viewProducts";
+            this.viewProducts.RowTemplate.Height = 25;
+            this.viewProducts.Size = new System.Drawing.Size(673, 306);
+            this.viewProducts.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.viewProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.viewProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView viewProducts;
+    }
+}
