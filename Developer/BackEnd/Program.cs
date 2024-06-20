8234d3334771ae38a86aa7de0add597a665b8875
jhondeiberd <70600455+jhondeiberd@users.noreply.github.com>
2024-06-20 12:07:24 -0400

    Add BackEnd project
    This project is for all the Tips

---
+++ Developer/BackEnd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HouseBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sfiles = "metacoll.updates.D20240613.T213016.WebsiteDUMP.3.mrc,metacoll.updates.D20240613.T213016.WebsiteDUMP.2.mrc";
+
+            var test11 = GetDateStringFromWMSzipFile(sfiles.Split(',')[0]);
+
+            var f3 = sfiles.Split(',')
+                .Select(x => new
+                {
+                    dateGroup = GetDateStringFromWMSzipFile(x),
+                    countGroup = x.Count()
+                })
+                .Where(x => x.countGroup > 2);
+
+            public static string GetDateStringFromWMSzipFile(string Filename)
+            {
+                return Filename.Split('.')[2];
+            }
+        }
+    }
+}
